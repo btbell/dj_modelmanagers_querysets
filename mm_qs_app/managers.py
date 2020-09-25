067b0799afb01e850ea7e43e9130f8a52f3298af
btbell <btbell@gmail.com>
2020-09-25 17:20:52 -0400

    css tweaks

--- mm_qs_app/managers.py
+++ mm_qs_app/managers.py
@@ -1,22 +1,13 @@
 from django.db import models
 
 class PostQuerySet(models.QuerySet):
-    def smaller_than(self, size):
-        return self.filter(comments__lt=size)
-
-    def greater_than(self, size):
-        return self.filter(comments__gt=size)
-
     def get_users_posts(self, username):
-        return self.filter(author__username=username)
+        return self.filter(author__user__username=username)
 
 class PostManager(models.Manager):
     def get_queryset(self):
         return PostQuerySet(self.model, using=self._db)
 
-    def smaller_than(self, size):
-        return self.filter(comments__lt=size)
-
-    def brians_posts(self):
-        return self.get_queryset().get_brians_posts()
+    def get_users_posts(self, username):
+        return self.get_queryset().get_users_posts(username)
 
--- mm_qs_app/views.py
+++ mm_qs_app/views.py
@@ -4,10 +4,10 @@ from .models import Post, Author
 
 def posts_list(request):
     all_posts = Post.objects.all()
-    #admins_posts = Post.objects.get_users_posts('admin')
+    admins_posts = Post.objects.get_users_posts('admin')
     context = {
         'all_posts': all_posts,
-        #'admins_posts': admins_posts
+        'admins_posts': admins_posts
     }
     #messages.info(request, 'Here are all the posts')
     return render(request, 'mm_qs_app/post_list.html', context)
